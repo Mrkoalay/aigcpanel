2d5172e1066ffa572a670d29840ebcb8c252f259
Mrkoalay <494150123@qq.com>
2026-02-27 17:15:02 +0800

    feat(go): migrate model registry storage to sqlite

---
+++ go/internal/domain/LocalModelRegistry.go
@@ -0,0 +1,20 @@
+package domain
+
+type LocalModelRegistryModel struct {
+	ID        int64  `gorm:"column:id;primaryKey;autoIncrement" json:"id"`
+	Key       string `gorm:"column:key;type:text;uniqueIndex" json:"key"`
+	Name      string `gorm:"column:name;type:text" json:"name"`
+	Title     string `gorm:"column:title;type:text" json:"title"`
+	Version   string `gorm:"column:version;type:text" json:"version"`
+	Type      string `gorm:"column:type;type:text" json:"type"`
+	AutoStart bool   `gorm:"column:autoStart" json:"autoStart"`
+	Functions string `gorm:"column:functions;type:text" json:"functions"`
+	LocalPath string `gorm:"column:localPath;type:text" json:"localPath"`
+	Settings  string `gorm:"column:settings;type:text" json:"settings"`
+	Setting   string `gorm:"column:setting;type:text" json:"setting"`
+	Config    string `gorm:"column:config;type:text" json:"config"`
+}
+
+func (LocalModelRegistryModel) TableName() string {
+	return "local_model_registry"
+}
